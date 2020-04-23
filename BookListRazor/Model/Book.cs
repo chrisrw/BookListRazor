dedc1958928fa4de6293994e8849093b5c7d1aaa
Chris Wargo <Crwargo0783@gmail.com>
2020-04-23 15:24:06 -0400

    added Book model class, connection string and packages, book table to SQL database

---
+++ BookListRazor/Model/Book.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookListRazor.Model
+{
+    public class Book
+    {
+        // automatically add id as an identity column. creates value automatically   
+        [Key]
+        public int Id { get; set; }
+        // make a name required
+        [Required]
+        public string Name { get; set; }
+        public string Author { get; set; }
+    }
+}
